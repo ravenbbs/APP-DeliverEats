d8f443eb60940db8606d76f8f02b99da3f4013ed
Julio Condor <julioc3bgud@gmail.com>
2023-11-19 14:37:27 -0500

    Cambio nombres salad, agregando detalles HomeMenu

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -1,11 +1,16 @@
+import Image from 'next/image'
 export default function HomeMenu(){
   return(
     <section className="">
+      <div className=''>
+        <Image scr={'../../../public/salad-left.png'}  alt={'lechuga'}/>
+
+      </div>
       <div className="text-center font-semibold">
-        <h3 className="text-gray-600 text-xl">
+        <h3 className="text-gray-600 text-xl leading-3">
           Check Out
         </h3>
-        <h2>
+        <h2 className="text-customColor font-bold text-4xl italic">
           Menu
         </h2>
       </div>

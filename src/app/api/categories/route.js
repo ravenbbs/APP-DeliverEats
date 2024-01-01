b68baea030b30a6d58d2e7ff5506fb974a94fe71
Julio Condor <julioc3bgud@gmail.com>
2024-01-01 00:43:49 -0500

    Category to DB

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -1,4 +1,15 @@
+import {Category} from '../../models/Category'
+
 export async function POST(req){
   const {name} = await req.json();
-  return
+  const categoryDoc = await Category.create({name})
+  return Response.json(categoryDoc)
+
+}
+
+
+export async function GET(){
+  return Response.json (
+    await Category.find()
+    ) 
 }

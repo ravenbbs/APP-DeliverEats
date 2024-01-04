48dc2784cb36a28192ed9eb2bf8b71142226bbe1
Julio Condor <julioc3bgud@gmail.com>
2024-01-03 23:32:17 -0500

    Agregando comentarios

--- src/app/models/MenuItem.js
+++ src/app/models/MenuItem.js
@@ -1,8 +1,20 @@
-import {model, models, Schema} from "mongoose";
+// Importa 'model', 'models' y 'Schema' desde la biblioteca Mongoose.
+import { model, models, Schema } from "mongoose";
 
-const MenuItemsSchema = new Schema({
-  image: {type: String},
-  name:{type:String},
-  description:{type:String},
-  price:{type:String},
-}, {timestamps: true})
+// Define un esquema (schema) para los elementos del menú.
+const MenuItemSchema = new Schema({
+    // Propiedad para la ruta de la imagen del elemento del menú.
+    image: { type: String },
+
+    // Propiedad para el nombre del elemento del menú.
+    name: { type: String },
+
+    // Propiedad para la descripción del elemento del menú.
+    description: { type: String },
+
+    // Propiedad para el precio del elemento del menú.
+    price: { type: String },
+}, { timestamps: true });  // timestamps: true añade automáticamente campos 'createdAt' y 'updatedAt' al esquema.
+
+// Crea el modelo 'MenuItem' utilizando el esquema definido anteriormente.
+export const MenuItem = models?.MenuItem || model('MenuItem', MenuItemSchema); // es una forma de verificar si el modelo ya existe.

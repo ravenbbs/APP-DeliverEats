e2722e18d6519fc0c09e30f91986563a3781551c
Julio Condor <julioc3bgud@gmail.com>
2024-01-04 01:47:09 -0500

    Menu item page GET - New menu items

--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -3,37 +3,11 @@ import { useEffect, useState } from "react";
 import { useProfile } from "../../components/UseProfile";
 import Loading from "../../components/layout/Loading";
 import UserTabs from "../../components/layout/UserTabs";
-import EditableImage from "../../components/layout/EditableImage";
-import toast from "react-hot-toast";
+
 
 export default function MenuItemsPAge() {
-  const [image, setImage] = useState("");
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [price, setPrice] = useState("");
-
   // Utiliza el hook useProfile para gestionar el estado del perfil, incluyendo el estado de carga y los datos del perfil.
   const { loading: profileLoading, data: profileData } = useProfile();
-
-  async function handleFormSubmit(ev) {
-    ev.preventDefault();
-    const savingPromise = new Promise(async (resolve, reject) => {
-      const data = { image, name, description, price };
-      const response = await fetch("/api/menu-items", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: { "Content-Type": "application/json" },
-      });
-      if (response.ok) resolve();
-      else reject();
-    });
-
-    await toast.promise(savingPromise, {
-      loading: "Cargando ...",
-      success: "Producto guardado!!",
-      error: "Ocurrió un error. Intenta de nuevo más tarde.",
-    });
-  }
 
   if (profileLoading) {
     return <Loading />;
@@ -43,48 +17,9 @@ export default function MenuItemsPAge() {
   }
 
   return (
-    <section className="max-w-xl mx-auto my-8">
-      <UserTabs isAdmin={true} />
-      <div className="max-w-xl mx-auto my-8">
-        <form onSubmit={handleFormSubmit} className=" mx-auto my-8">
-          <div className="flex gap-4">
-            <div>
-              <EditableImage
-                link={image}
-                setLink={setImage}
-                width={120}
-                height={120}
-              />
-            </div>
-            <div className=" grow ">
-              <label>Nombre</label>
-              <input
-                required={true}
-                value={name}
-                onChange={(ev) => setName(ev.target.value)}
-                type="text"
-              />
-              <label>Descripción </label>
-              <input
-                required={true}
-                value={description}
-                onChange={(ev) => setDescription(ev.target.value)}
-                type="text"
-              />
-              <label>Precio</label>
-              <input
-                required={true}
-                value={price}
-                onChange={(ev) => setPrice(ev.target.value)}
-                type="text"
-              />
-              <button className="mt-4 w-52 mx-auto " type="submit">
-                Guardar
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
+    <section className="max-w-md mx-auto my-8">
+    <UserTabs isAdmin={true} />
+    <div>menu</div>
     </section>
   );
 }

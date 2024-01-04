e28984023fbcaf6cc71651f1179a7f8411190e9b
Julio Condor <julioc3bgud@gmail.com>
2024-01-04 02:19:36 -0500

    Menu items link to new items

--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -3,9 +3,9 @@ import { useEffect, useState } from "react";
 import { useProfile } from "../../components/UseProfile";
 import Loading from "../../components/layout/Loading";
 import UserTabs from "../../components/layout/UserTabs";
+import Link from "next/link";
 
-
-export default function MenuItemsPAge() {
+export default function MenuItemsPage() {
   // Utiliza el hook useProfile para gestionar el estado del perfil, incluyendo el estado de carga y los datos del perfil.
   const { loading: profileLoading, data: profileData } = useProfile();
 
@@ -18,8 +18,15 @@ export default function MenuItemsPAge() {
 
   return (
     <section className="max-w-md mx-auto my-8">
-    <UserTabs isAdmin={true} />
-    <div>menu</div>
+      <UserTabs isAdmin={true}  />
+      <div className="mt-8">
+        <Link 
+          className="block text-center text-black buttonCustom bg-customColorBT" 
+          href={"/menu-items/new"}>
+            Crear Nuevo
+        </Link>
+      </div>
+        
     </section>
   );
 }

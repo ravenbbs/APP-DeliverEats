47290c0288b0e8ab8ce31e56a4b52dcf8f08c8c6
Julio Condor <julioc3bgud@gmail.com>
2024-01-03 20:16:42 -0500

    Add states  for menu items

--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -7,8 +7,17 @@ import EditableImage from "../../components/layout/EditableImage";
 
 export default function MenuItemsPAge() {
   const [image, setImage] = useState("");
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const [price, setPrice] = useState('')
+
   // Utiliza el hook useProfile para gestionar el estado del perfil, incluyendo el estado de carga y los datos del perfil.
   const { loading: profileLoading, data: profileData } = useProfile();
+
+  async function handleFormSubmit(ev){
+    ev.preventDefault()
+  }
+
 
   // if (profileLoading) {
   //   return <Loading />;
@@ -21,18 +30,27 @@ export default function MenuItemsPAge() {
     <section className="max-w-xl mx-auto my-8">
       <UserTabs isAdmin={true} />
       <div className="max-w-xl mx-auto my-8">
-      <form className=" mx-auto my-8">
+      <form onSubmit={handleFormSubmit} className=" mx-auto my-8">
         <div className="flex gap-4">
           <div>
             <EditableImage link={image} setLink={setImage} width={120} height={120} />
           </div>
           <div className=" grow ">
             <label>Nombre</label>
-            <input type="text" />
+            <input 
+            value={name}
+            onChange={ev => setName(ev.target.value)}
+            type="text" />
             <label>Descripción </label>
-            <input type="text" />
+            <input 
+            value={description}
+            onChange={ev => setDescription(ev.target.value)}
+            type="text" />
             <label>Precio</label>
-            <input type="text" />
+            <input 
+            value={price}
+            onChange={ev => setPrice(ev.target.value)}
+            type="text" />
             <button className="mt-4 w-52 mx-auto " type="submit">
               Guardar
             </button>

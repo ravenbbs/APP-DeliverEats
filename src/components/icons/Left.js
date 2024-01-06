a719f40702c0b968894e96fb4ea6e43f5d2fba05
Julio Condor <julioc3bgud@gmail.com>
2024-01-06 00:31:09 -0500

    Add icon left

---
+++ src/components/icons/Left.js
@@ -0,0 +1,18 @@
+export default function Right({ className = "w-6 h-6" }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5"
+      />
+    </svg>
+  );
+}
